93293c4871a62f01eb8052477471e3487f3a54fe
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-03-10 17:19:35 +0600

    2nd question solved

--- query.sql
+++ query.sql
@@ -75,3 +75,18 @@ where genre.name like 'Rock'
 )
 order by email
 
+
+
+/*Q2: Let's invite the artists who have written the most rock music in our dataset. Write a
+query that returns the Artist name and total track count of the top 10 rock bands*/
+
+
+select artist.artist_id, artist.name, count(artist.artist_id) as total_songs
+from track
+join album on album.album_id = track.album_id
+join artist on artist.artist_id = album.artist_id
+join genre on genre.genre_id = track.genre_id
+where genre.name like 'Rock'
+group by artist.artist_id
+order by total_songs desc
+limit 10
